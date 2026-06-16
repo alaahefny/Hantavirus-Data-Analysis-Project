82c2ef2a162376a4c4a9b5ba242e5b1427bb1a4c
alaahefny <alaahefny831@gmail.com>
2026-06-16 23:38:29 +0300

    Delete advanced_sql_queries.sql

--- advanced_sql_queries.sql
+++
@@ -1,9 +0,0 @@
-use moviesdb
-select PLATFORM,MAX(weekend_screen_hours)
-from dbo.screen_time_behavior
-where   country in(
-select country
-from dbo.country_wise_analysis_addiction
-where addiction_score>50 and gender='male' )
-group by  platform
-having MAX(weekend_screen_hours)>13
